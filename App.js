e107fdcd7fa1bd03ad7d13317266defa08e5965a
Your Name <danial1029@hotmail.com>
2020-10-30 12:29:12 +0200

    save

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,3 +19,4 @@ const styles = StyleSheet.create({
   },
 });
 // work ?????
+//dsadsad
