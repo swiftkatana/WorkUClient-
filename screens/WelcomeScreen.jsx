aab0e82bc73c99fae17b814324d4decd403742e1
Your Name <danial1029@hotmail.com>
2020-10-30 11:56:22 +0200

    add a screen window

---
+++ screens/WelcomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
