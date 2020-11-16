c8a00837c79cf6edcca2857c6b5766c2520e7a2d
unknown <koreru96@gmail.com>
2020-11-16 23:43:22 +0200

    1

--- app/src/api/serverIP.js
+++ app/src/api/serverIP.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
 const outip = "https://worku.herokuapp.com"
-const inMyServer = 'http://84.108.77.60:1029'
-const ip = outip;
+const inMyServer = 'http://84.108.77.86:1029'
+const ip = inMyServer;
 export default ip;
