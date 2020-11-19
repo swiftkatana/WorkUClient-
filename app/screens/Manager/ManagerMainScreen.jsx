246458b69ef6344e401835bf644c191686389f18
Your Name <danial1029@hotmail.com>
2020-11-19 12:36:34 +0200

    add handlers for new notification on manager and employee

--- app/screens/Manager/ManagerMainScreen.jsx
+++ app/screens/Manager/ManagerMainScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Alert } from 'react-native'
 import Greeting from "../../components/Greeting"
 import CompanyCode from "../../components/CompanyCode"
 import TaskBoard from "../../components/TaskBoard"
@@ -9,9 +9,29 @@ import * as Notifications from "expo-notifications";
 export default function Main({ navigation }) {
 
 
+
     useEffect(() => {
         const subscription = Notifications.addNotificationReceivedListener(notification => {
-            console.log(notification);
+            let { data, body } = notification.request.content;
+            console.log(notification.request.content)
+            switch (body) {
+
+                case 'updateTask':
+                    Alert.alert('you got notification', body);
+                    delete globalObject.User.tasks.processing[data._id]
+                    globalObject.User.tasks.completed[data._id] = data;
+                    break;
+
+                case 'newPersonalRequest':
+                    Alert.alert('you got notification', body);
+                    globalObject.User.personalRequests[data._id] = data;
+
+                    break;
+
+                default:
+                    Alert.alert('you got not handler notification', body);
+                    break;
+            }
         });
         return () => subscription.remove();
     }, []);
