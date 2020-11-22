48080b3104ce0a6200f1a84115342422145f99de
unknown <koreru96@gmail.com>
2020-11-22 20:25:04 +0200

    1

--- app/components/ManagerNaviButton.jsx
+++ app/components/ManagerNaviButton.jsx
@@ -59,7 +59,7 @@ const styles = StyleSheet.create({
     {
         //marginHorizontal:1,
         borderRadius: 30,
-        width: 75,
+        width: 79,
         height: 70,
         justifyContent: 'center',
         alignItems: 'center',
