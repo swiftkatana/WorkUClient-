0c5cab9ca7173f233c05e75850c3ec5454aec4f0
Daniel Levy <58981441+swiftkatana@users.noreply.github.com>
2020-11-24 22:52:06 +0200

    fix the filter for the manager

--- app/components/TaskBoard.jsx
+++ app/components/TaskBoard.jsx
@@ -1,121 +1,143 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { globalObject } from '../src/globalObject';
-import InfoList from '../components/InfoList';
-
-
+import React from "react";
+import { StyleSheet, Text, View, Dimensions, Image } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { globalObject } from "../src/globalObject";
+import InfoList from "../components/InfoList";
 
 export default function TaskBoard({ navigation }) {
-
-    const render = ({ item }) => {
-        return (
-            <View>
-                <TouchableOpacity style={styles.list} onPress={() => navigation.navigate('TaskUpdateVoiceScreen', { item: item,shouldRender:true })}>
-                    <Text style={styles.listText}>תקציר: {item.title}</Text>
-                    <View style={styles.koral}>
-                        <Image style={styles.tinyLogo} source={require('../assets/arrow_icon_black.png')} />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        )
-    }
-    const GetLen = () => {
-        return Object.keys(globalObject.User.tasks.processing).length;
-    }
-    const GetList = () => {
-        let arr = [];
-        for (var obj in globalObject.User.tasks.processing) {
-            arr.push(globalObject.User.tasks.processing[obj]);
-        }
-        return arr;
-    }
-
+  const render = ({ item }) => {
     return (
-        <View style={styles.view}>
-            <Text style={styles.boardTitle}>
-                לוח משימות
-            </Text>
-            <View style={{ flex: 1, }}>
-                <InfoList render={render} GetLen={GetLen} GetList={GetList} emptyInfo={'אין משימות'} textColor={"grey"} opacity={0.4} src={require('../assets/empty_icon.png')} />
-            </View>
-        </View>
-    )
+      <View>
+        <TouchableOpacity
+          style={styles.list}
+          onPress={() =>
+            navigation.navigate("TaskUpdateVoiceScreen", {
+              item: item,
+              shouldRender: true,
+            })
+          }
+        >
+          <Text style={styles.listText}>תקציר: {item.title}</Text>
+          <View style={styles.koral}>
+            <Image
+              style={styles.tinyLogo}
+              source={require("../assets/arrow_icon_black.png")}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  };
+  const GetLen = () => {
+    return Object.keys(globalObject.User.tasks.processing).length;
+  };
+  const GetList = () => {
+    let arr1 = [];
+    let arr2 = [];
+    let arr3 = [];
+    for (var obj in globalObject.User.tasks.processing) {
+      switch (globalObject.User.tasks.processing[obj].priority) {
+        case "גבוהה":
+          arr1.push(globalObject.User.tasks.processing[obj]);
+          break;
+        case "בינונית":
+          arr2.push(globalObject.User.tasks.processing[obj]);
+
+          break;
+        case "נמוכה":
+          arr3.push(globalObject.User.tasks.processing[obj]);
+          break;
+        default:
+          break;
+      }
+    }
+    return [...arr1, ...arr2, ...arr3];
+  };
+
+  return (
+    <View style={styles.view}>
+      <Text style={styles.boardTitle}>לוח משימות</Text>
+      <View style={{ flex: 1 }}>
+        <InfoList
+          render={render}
+          GetLen={GetLen}
+          GetList={GetList}
+          emptyInfo={"אין משימות"}
+          textColor={"grey"}
+          opacity={0.4}
+          src={require("../assets/empty_icon.png")}
+        />
+      </View>
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    view:
-    {
-        flex: 9,
-        height: Dimensions.get('window').height,
-        alignItems: 'center',
-        borderTopRightRadius: 20,
-        borderTopLeftRadius: 90,
-        marginHorizontal: 5,
-        borderBottomLeftRadius: 5,
-        borderBottomRightRadius: 5,
-    },
-    listText:
-    {
-        flex: 4,
-        textAlign: "right",
-        marginRight: 15,
-        fontSize: 14,
-    },
-    list:
-    {
-        height: 80,
-        width: Dimensions.get('window').width - 60,
-        backgroundColor: "white",
-        flexDirection: "row-reverse",
-        alignItems: 'center',
-        marginHorizontal: 20,
-        borderRadius: 25,
-        marginBottom: 10,
-        borderWidth: 1,
-        borderColor: "lightgrey",
-    },
-    boardTitle:
-    {
-        width: Dimensions.get('window').width - 10,
-        textAlign: "right",
-        marginRight: 90,
-        marginTop: 10,
-        fontSize: 18,
-        paddingVertical: 10,
-        borderRadius: 20,
-        marginBottom: 10,
-        color: "grey",
-        fontWeight: "bold",
-    },
-    logo: {
-        alignItems: 'flex-start',
-        justifyContent: 'flex-start',
-    },
-    tinyLogo: {
-        width: 20,
-        height: 20,
-        marginLeft: 15,
-        opacity: 0.7,
-    },
-    but:
-    {
-        width: 50,
-        height: 30,
-        marginHorizontal: 15,
-        backgroundColor: '#7f71e3',
-        textAlign: "center",
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 10,
-        marginVertical: 3,
-    },
-    butText:
-    {
-        color: "seashell",
-    },
-    koral:
-    {
-        flex: 1,
-        flexDirection: "column",
-    },
-})
+  view: {
+    flex: 9,
+    height: Dimensions.get("window").height,
+    alignItems: "center",
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 90,
+    marginHorizontal: 5,
+    borderBottomLeftRadius: 5,
+    borderBottomRightRadius: 5,
+  },
+  listText: {
+    flex: 4,
+    textAlign: "right",
+    marginRight: 15,
+    fontSize: 14,
+  },
+  list: {
+    height: 80,
+    width: Dimensions.get("window").width - 60,
+    backgroundColor: "white",
+    flexDirection: "row-reverse",
+    alignItems: "center",
+    marginHorizontal: 20,
+    borderRadius: 25,
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: "lightgrey",
+  },
+  boardTitle: {
+    width: Dimensions.get("window").width - 10,
+    textAlign: "right",
+    marginRight: 90,
+    marginTop: 10,
+    fontSize: 18,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginBottom: 10,
+    color: "grey",
+    fontWeight: "bold",
+  },
+  logo: {
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  tinyLogo: {
+    width: 20,
+    height: 20,
+    marginLeft: 15,
+    opacity: 0.7,
+  },
+  but: {
+    width: 50,
+    height: 30,
+    marginHorizontal: 15,
+    backgroundColor: "#7f71e3",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginVertical: 3,
+  },
+  butText: {
+    color: "seashell",
+  },
+  koral: {
+    flex: 1,
+    flexDirection: "column",
+  },
+});
